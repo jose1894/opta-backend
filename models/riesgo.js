6dda438d13e28c23803b41b8af31988c7bfe1b39
Maria_alvarez <marialva1705@gmail.com>
2023-08-19 20:13:12 -0400

    se modifica modelo y controlador de riesgos

--- models/riesgo.js
+++ models/riesgo.js
@@ -12,10 +12,182 @@ const RiesgoSchema = Schema({
     },
     descripcion: {
         type: String,
+        default: '',
     },
     indice: {
-        type: String,
+        type: Schema.Types.ObjectId,
+        ref: 'Enfoque',
     },
+    riesgoProveniente: {
+        type: String,
+        default: '',
+        enum: ['Transacciones Recurrentes','Transacciones no Recurrentes', 'Juicios y Estimaciones','accountingClosingEntries']
+    },
+    cuadrante: {
+        type: Number,
+        default: 0,
+        enum: [0,1,2,3,4]
+    },
+    areaRiesgo: {
+        type: Number,
+        default: 0,
+        enum: [0,1,2],
+    },
+    expectativasNegocio: {
+        type: String,
+        default: '',
+        enum: ['Intencionales','No Intencionales','Viabilidad del negocio','Expectativas del cliente']
+    },
+    procedimientosAdicionales: {
+        type: String,
+        default: '',
+        enum: ['Si','No']
+    },
+    inherente: {
+        type: String,
+        default: '',
+        enum: ['Medio', 'Bajo', 'Alto']
+    },
+    control: {
+        type: String,
+        default: '',
+        enum: ['Medio', 'Bajo', 'Alto']
+    },
+    analitico: {
+        type: String,
+        default: '',
+        enum: ['Medio', 'Bajo', 'Alto']
+    },
+    factorRiesgo: {
+        type: String,
+        default: '',
+    },
+    procesosInvolucrados: {
+        type: String,
+        default: '',
+    },
+    fuentesCausantes: {
+        type: String,
+        default: '',
+    },
+    consecuenciaEF: {
+        type: String,
+        default: '',
+    },
+    ase_a1: {
+        type: String,
+        default: '',
+    },
+    ase_a2: {
+        type: String,
+        default: '',
+    },
+    ase_a3: {
+        type: String,
+        default: '',
+    },
+    ase_a4: {
+        type: String,
+        default: '',
+    },
+    ase_a5: {
+        type: String,
+        default: '',
+    },
+     ase_b1: {
+        type: String,
+        default: '',
+    },
+    ase_b2: {
+        type: String,
+        default: '',
+    },
+    ase_b3: {
+        type: String,
+        default: '',
+    },
+    ase_b4: {
+        type: String,
+        default: '',
+    },
+    ase_c1: {
+        type: String,
+        default: '',
+    },
+    ase_c2: {
+        type: String,
+        default: '',
+    },
+    ase_c3: {
+        type: String,
+        default: '',
+    },
+    ase_c4: {
+        type: String,
+        default: '',
+    },
+    sel_mon: {
+        type: String,
+        default: '',
+        enum: ['No aprobado','Si','No']
+    },
+    sel_mon2: {
+        type: String,
+        default: '',
+        enum: ['No aprobado','Si','No']
+    },
+    sel_gen: {
+        type: String,
+        default: '',
+        enum: ['No aprobado','Si','No']
+    },
+    sel_gen2: {
+        type: String,
+        default: '',
+        enum: ['No aprobado','Si','No']
+    },
+    sel_esp: {
+        type: String,
+        default: '',
+        enum: ['No aprobado','Si','No']
+    },
+    sel_esp2: {
+        type: String,
+        default: '',
+        enum: ['No aprobado','Si','No']
+    },
+
+    sel2_ini: {
+        type: String,
+        default: '',
+        enum: ['No aprobado','Efectivo','No efectivo']
+    },
+
+    padc_enf: {
+        type: String,
+        default: ''
+    },
+
+    padc_res: {
+        type: String,
+        default: ''
+    },
+
+    referencia: {
+        type: String,
+        default: ''
+    },
+
+    pfo_mpro: {
+        type: String,
+        default: ''
+    },
+
+    rda_resi: {
+        type: String,
+        default: ''
+    },
+    
     estado : {
         type: Number,
         default: 1,
