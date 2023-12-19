c43f787d9708f18f017dda4546e3c2628e888e11
Jose Guerra <jose1894@gmail.com>
2023-12-18 21:19:04 -0400

    Subir cambios de frontend de diciembre

---
+++ public/assets/CardBoxComponentEmpty.46e8d214.js
@@ -0,0 +1,1 @@
+import{b as e}from"./BaseButton.fb27d38b.js";import{o as t,h as o,b as s}from"./index.97cc49ea.js";const r={},c={class:"text-center py-24 text-gray-500 dark:text-slate-400"},n=s("p",null,"Nothing's here\u2026",-1),a=[n];function _(d,i){return t(),o("div",c,a)}const h=e(r,[["render",_]]);export{h as C};
